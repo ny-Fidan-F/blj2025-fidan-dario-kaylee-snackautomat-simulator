8208e415966cba741512cb8b709c8aa12ee6e702
ny-Fidan-F <fidan.fetahi@noseryoung.ch>
2026-04-15 10:43:02 +0200

    Add setters for name, price and stock in Product

--- src/ch/noseryoung/blj/Product.java
+++ src/ch/noseryoung/blj/Product.java
@@ -21,11 +21,23 @@ public class Product {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public double getPrice() {
         return price;
     }
 
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
     public int getStock() {
         return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
     }
 }
