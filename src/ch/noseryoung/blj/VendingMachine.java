79156ad8767d8195f828fffa604bdd7b0de3b175
ny-dario-z <dario.zueger@noseryoung.ch>
2026-04-15 12:00:47 +0200

    added refill method + products

--- src/ch/noseryoung/blj/VendingMachine.java
+++ src/ch/noseryoung/blj/VendingMachine.java
@@ -53,8 +53,14 @@ public class VendingMachine {
     public void initialFill() {
         if (!this.initialFillDone) {
             products.add(new Product("A1", "Cola", 2.50, 10));
-            products.add(new Product("A2", "Chips", 1.80, 7));
-            products.add(new Product("A3", "Mars-Riegel", 1.50, 8));
+            products.add(new Product("A2", "Eistee", 2.30, 10));
+            products.add(new Product("A3", "Wasser", 1.50, 15));
+            products.add(new Product("B1", "Snickers", 1.80, 12));
+            products.add(new Product("B2", "Twix", 1.80, 12));
+            products.add(new Product("B3", "Gummibärchen", 2.10, 8));
+            products.add(new Product("C1", "Chips", 2.00, 7));
+            products.add(new Product("C2", "Salzstangen", 1.70, 10));
+            products.add(new Product("C3", "Apfelschnitze", 2.40, 5));
 
             this.initialFillDone = true;
             System.out.println("Automat wurde gefüllt.");
@@ -70,6 +76,13 @@ public class VendingMachine {
             }
         }
         return null;
+    }
+
+    public void refill(String code, int quantity) {
+        Product p = findProduct(code);
+        if (p != null) {
+            p.setStock(p.getStock() + quantity);
+        }
     }
 
     public boolean replaceProduct(String code, String newName, double newPrice, int newStock) {
