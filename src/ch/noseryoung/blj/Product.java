7723600b8aec19af7a3fedb3f0f273378ab228dd
ny-Fidan-F <fidan.fetahi@noseryoung.ch>
2026-04-15 10:35:26 +0200

    Add constructor to Product

--- src/ch/noseryoung/blj/Product.java
+++ src/ch/noseryoung/blj/Product.java
@@ -6,4 +6,10 @@ public class Product {
     private double price;
     private int stock;
 
+    public Product(String code, String name, double price, int stock) {
+        this.code = code;
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+    }
 }
