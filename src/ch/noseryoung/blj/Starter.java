67cb1c250f3e5985dc003b947142a00b1acaf962
ny-kaylee-o <kaylee.osagie@noseryoung.ch>
2026-04-15 09:54:52 +0200

    Fixed file directory (again)

--- src/ch/noseryoung/blj/Starter.java
+++ src/ch/noseryoung/blj/Starter.java
@@ -1,50 +1,8 @@
 package ch.noseryoung.blj;
 
-import java.util.InputMismatchException;
-import java.util.Scanner;
 
 public class Starter {
     static void main() {
-        int select = 0;
-        Scanner input = new Scanner(System.in);
-
-        System.out.println("hi");
-        System.out.println("-------------------------\n");
-        System.out.println("1 : Three-Two-One");
-        System.out.println("2 : Weihnachtsbaum");
-        System.out.println("3 : Stoffwechselrate");
-        System.out.println("0 : Quit");
-
-        try {
-            select = input.nextInt();
-            throw new RuntimeException("Kill yourself");
-        } catch (RuntimeException e) {
-            System.out.println(e.getMessage());
-        }
-
-        if (select == 1) {
-            new ThreeTwoOne().present();
-        }
-
-        if (select == 2) {
-            new Weihnachtsbaum().present();
-        }
-
-        if (select == 2) {
-            new Weihnachtsbaum().present();
-        }
-
-        if (select == 3) {
-            new Stoffwechselrate().present();
-        }
-
-        if (select == 4) {
-            new Baumstammvolumen().present();
-        }
-
-        if (select == 0) {
-            System.exit(0);
-        }
-
+        System.out.println("Hello World!");
     }
 }
--- src/ch/noseryoung/blj/ThreeTwoOne.java
+++
@@ -1,7 +0,0 @@
-package ch.noseryoung.blj;
-
-public class ThreeTwoOne {
-    public void present() {
-        System.out.print("Three-Two-One - Takeoff! ");
-    }
-}
